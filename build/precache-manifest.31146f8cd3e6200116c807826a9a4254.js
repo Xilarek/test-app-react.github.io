aa9d40815bc5a8f5cde86d78b6969d70027eac56
Dmitry Mikhaylov <59124227+Xilarek@users.noreply.github.com>
2020-08-20 11:28:24 +0300

    Add files via upload

---
+++ build/precache-manifest.31146f8cd3e6200116c807826a9a4254.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4379f537c296c3418332b9835f6670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3b3aa8a9f0a7c124ed",
+    "url": "/static/css/main.2251e88a.chunk.css"
+  },
+  {
+    "revision": "c6e768b28964c3dd3be2",
+    "url": "/static/js/2.e2a61d87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e2a61d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3b3aa8a9f0a7c124ed",
+    "url": "/static/js/main.b69242dc.chunk.js"
+  },
+  {
+    "revision": "3c723e48decc6f6f72c1",
+    "url": "/static/js/runtime-main.30aace72.js"
+  },
+  {
+    "revision": "de0db40d890968cacbabc05df9d612ef",
+    "url": "/static/media/Avatar.de0db40d.png"
+  },
+  {
+    "revision": "3d34ae460648f82d3c1b07d0982d61cc",
+    "url": "/static/media/arrow-down.3d34ae46.svg"
+  },
+  {
+    "revision": "b801188b37a89e0688891529f31670c0",
+    "url": "/static/media/arrow-left.b801188b.svg"
+  },
+  {
+    "revision": "f232111841184d610f54bc0da68350b3",
+    "url": "/static/media/arrow-right.f2321118.svg"
+  },
+  {
+    "revision": "5f9b4de43fd30bc4c0050d2cd262254b",
+    "url": "/static/media/arrow-up.5f9b4de4.svg"
+  },
+  {
+    "revision": "6c8bf936e120aebfe7951c656813a7d2",
+    "url": "/static/media/arrow.6c8bf936.svg"
+  },
+  {
+    "revision": "2e56a0552586264a8881a5a901f71133",
+    "url": "/static/media/background.2e56a055.png"
+  },
+  {
+    "revision": "a2b000d0fe8a48da94e6acb873498f09",
+    "url": "/static/media/crumbs-Button.a2b000d0.svg"
+  }
+]);
